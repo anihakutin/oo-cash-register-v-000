ea10461d51e03e9fa7c4b194de0fa54ce666811b
Heshie Brody <4hbrody@gmail.com>
2018-08-26 18:33:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
+# OO Cash Register
+# - Add items of varying quantities and prices
+# - Calculate discounts
+# - Keep track of what's been added to it
+# - Void the last transaction
 
+class CashRegister
+  attr_accessor :total, :discount, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = [ ]
+  end
+  def add_item(item, price, quantity = 1)
+    self.total += price * quantity
+    self.items += [item * quantity]
+  end
+  def apply_discount
+      if self.discount > 0
+        self.total -= self.discount.to_f * 10
+        "After the discount, the total comes to $#{self.total.to_i}."
+      else
+        "There is no discount to apply."
+      end
+  end
+
+end
